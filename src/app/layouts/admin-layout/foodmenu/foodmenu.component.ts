dd5cfa0502151b64cb46982aa04a3bf93ed41002
amanitek <amenychpro@gmail.com>
2024-04-11 21:47:28 +0100

    updated foodmenu

--- src/app/layouts/admin-layout/foodmenu/foodmenu.component.ts
+++ src/app/layouts/admin-layout/foodmenu/foodmenu.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Menu } from 'src/app/models/menu';
-import { Category } from '../category/category';
-import { MenuService } from 'src/app/__services/menu.service';
 
 @Component({
   selector: 'app-foodmenu',
@@ -9,33 +6,5 @@ import { MenuService } from 'src/app/__services/menu.service';
   styleUrls: ['./foodmenu.component.css']
 })
 export class FoodmenuComponent {
-  menus: Menu[] = [];
-    categories: Category[] = [];
-    constructor(private menuService: MenuService) { }
-    ngOnInit(): void {
-      this.getAllMenus();
-      this.getCategories();
-    }
-    getAllMenus() {
-      this.menuService.getAllMenus().subscribe(
-        (response: any) => { 
-          this.menus = response;
-        },
-        error => {
-          console.log('Error fetching menus: ', error);
-        }
-      );
-    }
-    
-    getCategories() {
-      this.menuService.getCategories().subscribe(
-        (response: Category[]) => {
-          this.categories = response;
-        },
-        error => {
-          console.log('Error fetching categories: ', error);
-        }
-      );
-    }
-    
+
 }

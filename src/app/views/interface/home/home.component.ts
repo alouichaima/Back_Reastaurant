47b6ff35077231f8a42a479d488682a8ceaf3d62
oumaima <oumaimaguedri66@gmail.com>
2024-04-15 13:55:49 +0200

    affichage menu pour client

--- src/app/views/interface/home/home.component.ts
+++ src/app/views/interface/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CategoryService } from 'src/app/__services/category.service';
 import { MenuitemService } from 'src/app/__services/menuitem.service';
 import { MenuItem } from 'src/app/models/menu-item';
 
@@ -8,22 +9,38 @@ import { MenuItem } from 'src/app/models/menu-item';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  listCategories:any;
+  listMenuItems:any;
+  // listMenuItems: MenuItem[] = [];
+  menuItem: MenuItem = {
+    id: null,
+    name: null,
+    description:null,
+    price: null,
+  };
 
-  listMenu:any;
-  menu!:MenuItem;
+  showAddMenuItemForm: boolean = false;
+
+  constructor(
+    private menuItemService: MenuitemService,
+    private categoryService:CategoryService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllMenuItems();
+  }
+  getAllCategories(){
+    this.categoryService.getCategories().subscribe(res => this.listCategories = res)
+  }
  
+  getAllMenuItems() {
+    this.menuItemService.getAllMenuItems().subscribe(res => this.listMenuItems = res)
+
+  }
+
   
 
 
-  constructor(private menuservice:MenuitemService){}
-  ngOnInit(): void {
-    this.getMenuItems();
-   
-  }
- 
-    getMenuItems(){
-    this.menuservice.getMenuItems().subscribe(res => this.listMenu = res)
-  }
 
 
 }
